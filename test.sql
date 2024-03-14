d8083d848529f450eb4afadc6be7447a9fff1e8d
ThienNguyen3001 <147313343+ThienNguyen3001@users.noreply.github.com>
2024-03-14 21:45:08 +0700

    Update at 9h45 14/3

--- test.sql
+++ test.sql
@@ -1,8 +1,8 @@
 ﻿-- Đưa ra thông tin cần thiết
 select location,date,total_cases,new_cases,total_deaths,population 
 from CovidDeaths
-where total_cases is not null
-and year(date) = 2024
+where year(date) > 2022
+and continent is not null
 and location like '%Viet%'
 order by 1,2
 
@@ -10,8 +10,8 @@ order by 1,2
 select Location,Date,Total_cases,Total_deaths,
 (total_deaths*1.0/total_cases)*100 as DeathPercencage
 from CovidDeaths
-where total_cases is not null and total_deaths is not null
-and year(date) = 2024
+where year(date) > 2022
+and continent is not null
 and Location like '%Viet%'
 order by 1,2
 
@@ -19,15 +19,29 @@ order by 1,2
 select Location,date,total_cases,Population,
 (total_cases*1.0/population)*100 as CasePercencage
 from CovidDeaths
-where total_cases is not null
-and year(date) = 2024
+where year(date) >2022
+and continent is not null
 and Location like '%Viet%'
 order by 1,2
 
--- Tổng lây nhiễm cao nhất so với dân số của thế giới
+-- Tổng lây nhiễm cao nhất so với dân số của từng quốc gia
 select Location,max(total_cases) HighestInfection,Population,
 max((total_cases*1.0/population))*100 as PercentIfected
 from CovidDeaths
-where total_cases is not null
+where continent is not null
 group by location,population
 order by 4 desc
+
+-- Tổng số người chết cao nhất mỗi quốc gia
+select Location,max(total_deaths) HighestDeathsCount
+from CovidDeaths
+where continent is not null
+group by location
+order by 2 desc
+
+-- Tổng số người chết cao nhất mỗi lục địa
+select Continent,max(total_deaths) HighestDeathsCount
+from CovidDeaths
+where continent is not null
+group by continent
+order by 2 desc
