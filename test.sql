47bab17339fcee5180bed1b52455377f34b0a343
ThienNguyen3001 <147313343+ThienNguyen3001@users.noreply.github.com>
2024-03-14 15:39:37 +0700

    Update 14/3 at 3h40

--- test.sql
+++ test.sql
@@ -7,19 +7,27 @@ and location like '%Viet%'
 order by 1,2
 
 -- Tỷ lệ người chết số với tổng ca bệnh
-select location,date,total_cases,total_deaths,
+select Location,Date,Total_cases,Total_deaths,
 (total_deaths*1.0/total_cases)*100 as DeathPercencage
 from CovidDeaths
 where total_cases is not null and total_deaths is not null
 and year(date) = 2024
-and location like '%Viet%'
+and Location like '%Viet%'
 order by 1,2
 
 -- Tỷ lệ lây nhiễm với dân số
-select location,date,total_cases,population,
+select Location,date,total_cases,Population,
 (total_cases*1.0/population)*100 as CasePercencage
 from CovidDeaths
 where total_cases is not null
 and year(date) = 2024
-and location like '%Viet%'
+and Location like '%Viet%'
 order by 1,2
+
+-- Tổng lây nhiễm cao nhất so với dân số của thế giới
+select Location,max(total_cases) HighestInfection,Population,
+max((total_cases*1.0/population))*100 as PercentIfected
+from CovidDeaths
+where total_cases is not null
+group by location,population
+order by 4 desc
